5532a423fb5fbc1c2aa98814cc9eb9a8b5ca5217
Andrexyxyx <108584545+Andrexyxyx@users.noreply.github.com>
2022-10-02 15:45:33 +0700

    Testing API

---
+++ app/PetFriends_Settings.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# valid_email = os.getenv('valid_email')
+# valid_password = os.getenv('valid_password')
+valid_email = "test_mail90@mail.ru"
+valid_password = "-andrew25-"
+
+invalid_email = 'badmail@yandex.ru'
+invalid_password = '1q2w3e4r'
